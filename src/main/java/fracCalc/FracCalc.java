a9187272b4923ecd395f287ac8e8c5dbca30fde5
s-yua <s-yua@53S500035875.it.bsd405.org>
2019-12-02 22:42:46 -0800

    my checkp in t 222 33333333eieiieieieiie
    33
    3
    33
    3
    3

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -5,201 +5,160 @@ import java.util.*;
 public class FracCalc {
 
 	public static void main(String[] args) {
-
 		Scanner userInput = new Scanner(System.in);
-
-		String userFractions = userInput.nextLine();
-
-		while (!(userFractions.toUpperCase()).equals("QUIT")) {
-
-			System.out.println(produceAnswer(userFractions));
-
-			userFractions = userInput.nextLine();
-
+		String fractions = userInput.nextLine();
+		while (!(fractions.toUpperCase()).equals("QUIT")) {
+			
+			
+			System.out.println(produceAnswer(fractions));
+			fractions = userInput.nextLine();
 		}
-
 		userInput.close();
 
 	}
 
 	public static String produceAnswer(String input) {
-
 		String operator;
-
 		int numsBefore;
-
 		int a = input.indexOf(" + ");
-
 		int b = input.indexOf(" - ");
-
 		int c = input.indexOf(" * ");
-
 		int d = input.indexOf(" / ");
-
 		if (a != -1) {
-
 			numsBefore = a;
-
 			operator = "+";
-
-		} else if (b != -1) {
-
+		}
+		else if (b != -1) {
 			numsBefore = b;
-
 			operator = "-";
-
-		} else if (c != -1) {
-
+		} 
+		else if (c != -1) {
 			numsBefore = c;
-
 			operator = "*";
-
-		} else {
-
+		} 
+		else {
 			numsBefore = d;
-
 			operator = "/";
-
 		}
-
-		String Fraction1 = "";
-
-		String Fraction2 = "";
-
+		String firstFraction = "";
+		String secondFraction = "";
 		for (int i = 0; i < numsBefore; i++) {
-
-			Fraction1 += input.charAt(i);
-
+			firstFraction = firstFraction + input.charAt(i);
 		}
-
 		for (int i = 0; i < input.length() - 3 - numsBefore; i++) {
-
-			Fraction2 += input.charAt(3 + numsBefore + i);
-
+			secondFraction = secondFraction + input.charAt(3 + numsBefore + i);
 		}
 
-		int Top1 = Integer.parseInt(whole(Fraction1));
+		int Hole1 = Integer.parseInt(whole(firstFraction));
+		int Hole2 = Integer.parseInt(whole(secondFraction));
+		int Numerator1 = Integer.parseInt(numerator(firstFraction));
+		int Numerator2 = Integer.parseInt(numerator(secondFraction));
+		int Denominator1 = Integer.parseInt(denominator(firstFraction));
+		int Denominator2 = Integer.parseInt(denominator(secondFraction));
 
-		int Top2 = Integer.parseInt(whole(Fraction2));
+		if (firstFraction.indexOf("-") != -1 && firstFraction.indexOf("_") != -1) {
+			Numerator1 = (Hole1 * Denominator1 - Numerator1);
+			if (operator.equals("+") || operator.equals("-")) {
+				Numerator1 *= Denominator2;
+			}
+		} else if (operator.equals("+") || operator.equals("-")) {
+			Numerator1 = (Numerator1 + Hole1 * Denominator1) * Denominator2;
+		} else {
+			Numerator1 = (Numerator1 + Hole1 * Denominator1);
+		}
 
-		int Numerator1 = Integer.parseInt(numerator(Fraction1));
+		if (secondFraction.indexOf("-") != -1 && secondFraction.indexOf("_") != -1) {
+			Numerator2 = (Hole2 * Denominator2 - Numerator2);
+			if (operator.equals("+") || operator.equals("-")) {
+				Numerator2 *= Denominator1;
+			}
+		} else if (operator.equals("+") || operator.equals("-")) {
+			Numerator2 = (Numerator2 + Hole2 * Denominator2) * Denominator1;
+		} else {
+			Numerator2 = (Numerator2 + Hole2 * Denominator2);
+		}
+		if (operator.equals("+") || operator.equals("-")) {
+			Denominator1 *= Denominator2;
+			Denominator2 = Denominator1;
+		}
 
-		int Numerator2 = Integer.parseInt(numerator(Fraction2));
+		int FinalNumerator = 0;
+		int FinalDenominator = 0;
+		if (operator.equals("+")) {
+			FinalNumerator = Numerator1 + Numerator2;
+			FinalDenominator = Denominator1;
+		} 
+		
+		else if (operator.equals("-")) {
+			FinalNumerator = Numerator1 - Numerator2;
+			FinalDenominator = Denominator1;
+		}
+			
+		
+		else if (operator.equals("/")) {
+				FinalNumerator = Numerator1 * Denominator2;
+				FinalDenominator = Denominator1 * Numerator2;
+		} 
+		
+		else {
+			FinalNumerator = Numerator1 * Numerator2;
+			FinalDenominator = Denominator1 * Denominator2;
+		}
 
-		int Denominator1 = Integer.parseInt(denominator(Fraction1));
-
-		int Denominator2 = Integer.parseInt(denominator(Fraction2));
-
-// sets up fractions for the calculation
-// sets up fractions for the calculation
-
-		return ("whole:" + Top2 + " " + "numerator:" + Numerator2 + " " + "denominator:"
-				+ Denominator2);
-
+		return FinalNumerator + "/" + FinalDenominator;
 	}
-
-//finds the whole number in a mixed or whole number
 
 	public static String whole(String fraction) {
-
 		String whole = "";
-
 		if (fraction.indexOf("/") == -1) {
-
 			whole = fraction;
-
 		} else if (fraction.indexOf("_") != -1) {
-
-			int beforeUnderscore = fraction.indexOf("_");
-
-			for (int i = 0; i < beforeUnderscore; i++) {
-
+			int numsBeforeUnderscore = fraction.indexOf("_");
+			for (int i = 0; i < numsBeforeUnderscore; i++) {
 				whole = whole + fraction.charAt(i);
-
 			}
-
 		} else if (fraction.indexOf("_") == -1) {
-
 			whole = "0";
-
 		}
-
 		return whole;
-
 	}
-
-//finds the numerator of a fraction
 
 	public static String numerator(String fraction) {
-
 		String numerator = "";
-
 		if (fraction.indexOf("/") == -1) {
-
 			numerator = "0";
-
 		} else if (fraction.indexOf("_") != -1) {
-
-			int beforeSlash = fraction.indexOf("/");
-
-			int beforeUnderscore = fraction.indexOf("_");
-
-			for (int i = 0; i < beforeSlash - beforeUnderscore - 1; i++) {
-
-				numerator = numerator + fraction.charAt(beforeUnderscore + 1 + i);
-
+			int numsBeforeSlash = fraction.indexOf("/");
+			int numsBeforeUnderscore = fraction.indexOf("_");
+			for (int i = 0; i < numsBeforeSlash - numsBeforeUnderscore - 1; i++) {
+				numerator = numerator + fraction.charAt(numsBeforeUnderscore + 1 + i);
 			}
-
 		} else if (fraction.indexOf("_") == -1) {
-
-			int beforeSlash = fraction.indexOf("/");
-
-			for (int i = 0; i < beforeSlash; i++) {
-
+			int numsBeforeSlash = fraction.indexOf("/");
+			for (int i = 0; i < numsBeforeSlash; i++) {
 				numerator = numerator + fraction.charAt(i);
-
 			}
-
 		}
-
 		return numerator;
-
 	}
-
-//finds the denominator of a fraction
 
 	public static String denominator(String fraction) {
-
 		String denominator = "";
-
 		if (fraction.indexOf("/") == -1) {
-
 			denominator = "1";
-
 		} else if (fraction.indexOf("_") != -1) {
-
-			int beforeSlash = fraction.indexOf("/");
-
-			for (int i = 0; i < fraction.length() - beforeSlash - 1; i++) {
-
-				denominator = denominator + fraction.charAt(beforeSlash + 1 + i);
-
+			int numsBeforeSlash = fraction.indexOf("/");
+			for (int i = 0; i < fraction.length() - numsBeforeSlash - 1; i++) {
+				denominator = denominator + fraction.charAt(numsBeforeSlash + 1 + i);
 			}
-
 		} else if (fraction.indexOf("_") == -1) {
-
-			int beforeSlash = fraction.indexOf("/");
-
-			for (int i = 0; i < fraction.length() - beforeSlash - 1; i++) {
-
-				denominator = denominator + fraction.charAt(beforeSlash + 1 + i);
-
+			int numsBeforeSlash = fraction.indexOf("/");
+			for (int i = 0; i < fraction.length() - numsBeforeSlash - 1; i++) {
+				denominator = denominator + fraction.charAt(numsBeforeSlash + 1 + i);
 			}
-
 		}
-
 		return denominator;
-
 	}
+	
 
 }
